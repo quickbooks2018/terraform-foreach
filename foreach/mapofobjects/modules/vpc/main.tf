8c70292afef2a5e66c562dc52e9a800ef4852a72
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2024-09-25 05:35:48 +0500

    Update main.tf

--- foreach/mapofobjects/modules/vpc/main.tf
+++ foreach/mapofobjects/modules/vpc/main.tf
@@ -1,4 +1,7 @@
 # https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/vpc
+
+# variables types
+# https://registry.terraform.io/modules/terraform-aws-modules/vpc/aws/latest
 resource "aws_vpc" "main" {
     for_each = var.vpcs
     cidr_block = each.value["cidr"]
